749dd5d31a9d12700d235451360fc6272c383916
Ryan de Morais Correia <ryan.morais.workspace@gmail.com>
2026-07-24 23:59:10 -0300

    docs: ansible modules defined and architecture updated

--- iac/diagrams/draw.py
+++ iac/diagrams/draw.py
@@ -1,31 +1,31 @@
-from diagrams import Diagram, Cluster
+from diagrams import Diagram, Cluster, Edge
 from diagrams.aws.network import Route53, ELB
 from diagrams.aws.compute import EC2, AutoScaling
+from diagrams.aws.management import Cloudwatch
 from diagrams.onprem.client import Users
 
 with Diagram(
-    "AWS Architecture",
+    "AWS Architecture - IFTech",
     filename="aws_architecture",
     show=False,
     direction="TB",
 ):
 
-    users = Users("IFTech GUESTS")
-
-    dns = Route53("Elastic IP\nRoute 53")
-
-    alb = ELB("Application\nLoad Balancer")
-
+    users = Users("IFTech\nGuests")
+    dns = Route53("Route 53")
+    
     with Cluster("VPC"):
+        alb = ELB("Application\nLoad Balancer")
+        cloudwatch = Cloudwatch("CloudWatch\nAlarms (CPU / ALB Req)")
 
-        with Cluster("AS Group\nMin:1 | Desired:1 | Max:2"):
+        with Cluster("Auto Scaling Group\n(Min: 1 | Max: 2)"):
+            asg = AutoScaling("Scaling Policy")
+            
+            ec2_a = EC2("Instance A\nApp + AI Model")
+            ec2_b = EC2("Instance B\nApp + AI Model")
+            
+            asg - [ec2_a, ec2_b]
 
-            asg = AutoScaling("Auto Scaling")
+    users >> dns >> alb >> ec2_a
 
-            ec2_a = EC2("Instance A\nApp Web\nLLM Server")
-
-            ec2_b = EC2("Instance B\nApp Web\nLLM Server")
-
-            asg >> [ec2_a, ec2_b]
-
-    users >> dns >> alb >> asg
+    cloudwatch >> Edge(color="firebrick", style="dashed", label="Triggers Scaling") >> asg
