2a5298f45596548c1e85d684916e9db5ee78fac7
Ryan de Morais Correia <ryan.morais.workspace@gmail.com>
2026-08-03 21:03:45 -0300

    feat: S3 bucket setup for .vmdk

---
+++ iac/terraform/modules/bucket/main.tf
@@ -0,0 +1,38 @@
+resource "aws_s3_bucket" "app_bucket" {
+  bucket = var.bucket_name
+
+  lifecycle {
+    prevent_destroy = false
+  }
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = var.environment
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket = aws_s3_bucket.app_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.app_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.app_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
---
+++ iac/terraform/modules/bucket/variables.tf
@@ -0,0 +1,7 @@
+variable "bucket_name" {
+  type        = string
+  default     = "iftech-app"
+}
+variable "environment" {
+  type        = string
+}
